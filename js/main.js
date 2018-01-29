b61fad25ea5a1ae742d577b2bd396e4b66c9d3ec
cmt1 <carlostorres@CARLOSs-MacBook-Air.local>
2018-01-28 18:42:00 -0800

    updates main.js

--- js/main.js
+++ js/main.js
@@ -6,36 +6,42 @@ let isInteger,
     fbText;
 
 
-// TODO: Initialize a variable `isInteger` to use as a control value for the
-// `while` loop. Set the initial value to `false`.
 
-// TODO: Create a `while` loop so user will be prompted to enter a number until
-// they enter a good number. This loop should be controlled by a conditional
-// expression that looks at the value of `isSafeInteger`. When `isSafeInteger`
-// becomes `true`, the `while` loop should stop looping.
+isInteger = false;
 
-// TODO: Inside `while` loop prompt the user for the `maxNumber` value.
 
-// TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
-// is a suitable integer. (It should be an integer greater than zero.)
+while (!isInteger) {
 
-// TODO: If the value of `maxNumber` is suitable, then change the value of
-// `isInteger` so the `while` loop stops looping.
+maxNumber = prompt ('Enter a number over zero(must be an integer):',20)
 
-// TODO: Initialize the `fbResults` variable to an empty Array
+maxNumber = parseInt(maxNumber);
+if (Number.isSafeInteger(maxNumber) &&maxNumber > 0){
+    isInteger = true;
+}
 
-// TODO: Create a `for` loop that will execute the `maxNumber` of times.
+}
 
-// TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
-// require the use of several conditional statements that use the `%` operator.
-// Store the results of this logic in an array called `fbResults`.
+fbResults = [];
 
-// TODO: In preparation for creating the output text for your FizzBuzz results,
-// initialize the `fbText` variable to an empty string.
 
-// TODO: Use a `for ... of` loop to concatenate the values from `fbResults`
-// into the variable `fbText`.
+for (let i=1; i <= maxNumber; i++) {
 
+if (i % 15 ==0) {
+    fbResults.push ('FizzBuzz');
+}   else if (i % 3 ==0) {
+    fbResults.push ('Fizz');
+}   else if (i % 5 ==0) {
+    fbResults.push('Buzz');
+} else {
+    fbResults.push(i);
+}
+}
+
+fbText = '';
+
+for (let result of fbResults) {
+    fbText = fbText + result + "\n"
+}
 
 ///////////////////////////////////////////////////////////////////////
 // Do not edit below this line unless you are doing something fancy!
